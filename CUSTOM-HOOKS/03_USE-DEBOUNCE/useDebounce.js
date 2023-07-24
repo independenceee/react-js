f518531aad1de7a90d5cf9263664ebc02a03f2d8
Independence <nguyenkhanh17112003@gmail.com>
2023-07-24 20:57:27 +0700

    custom hook useAsync

--- CUSTOM-HOOKS/03_USE-DEBOUNCE/useDebounce.js
+++ CUSTOM-HOOKS/03_USE-DEBOUNCE/useDebounce.js
@@ -1,8 +1,31 @@
-import { useEffect } from "react"
-import useTimeout from "../02_USE-TIMEOUT/useTimeout"
+import { useEffect } from "react";
+import useTimeout from "../02_USE-TIMEOUT/useTimeout";
 
 export default function useDebounce(callback, delay, dependencies) {
-  const { reset, clear } = useTimeout(callback, delay)
-  useEffect(reset, [...dependencies, reset])
-  useEffect(clear, [])
+  const { reset, clear } = useTimeout(callback, delay);
+  useEffect(reset, [...dependencies, reset]);
+  useEffect(clear, []);
 }
+
+
+
+//
+
+
+import { useState, useEffect } from "react";
+const useDebounces = function ({ value, delay }) {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(function () {
+      return setDebounceValue(value);
+    }, delay);
+
+    return function () {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debounceValue;
+};
+
+export { useDebounces };
